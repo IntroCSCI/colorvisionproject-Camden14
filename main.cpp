eb69d5a2c21570bb373474b025ef050afaa9ffea
Camden Ruinard <ccruinard@mail.csuchico.edu>
2021-03-12 07:44:29 +0000

    Asked for user to enter a file name.

--- main.cpp
+++ main.cpp
@@ -12,56 +12,68 @@ int main()
 
  cout << "Welcome to the Theme Completer. Here, you can enter a color and get back other colors that are color-blind compatible to the one entered." << endl;
 
-do {
+  do {
    cout << "Please enter a color, that is either Red, Yellow, Green, Blue, or Purple: " << endl;
- cin >> colorName;
+   
+   cin >> colorName;
 
-  if(colorName != "Red" && colorName != "Yellow" && colorName != "Green" && colorName != "Blue" && colorName != "Purple"){
-    cout << "The color you entered is not a valid color. Please enter one of the five colors listed. Be sure to capitalize the first letter of the color you enter." << endl;
-  }
+    if(colorName != "Red" && colorName != "Yellow" && colorName != "Green" && colorName != "Blue" && colorName != "Purple"){
 
-} while (colorName != "Red" && colorName != "Yellow" && colorName != "Green" && colorName != "Blue" && colorName != "Purple");
+      cout << "The color you entered is not a valid color. Please enter one of the five colors listed. Be sure to capitalize the first letter of the color you enter." << endl;
+    }
+
+  } while (colorName != "Red" && colorName != "Yellow" && colorName != "Green" && colorName != "Blue" && colorName != "Purple");
 
  cout << "The color you entered is: " << colorName << endl;
 
-if(colorName == "Red"){
-  
-  fileName = "RedColors.css";
-  
-  reader.open(fileName, ios::in);
-}
-  
-else if(colorName == "Yellow"){
-  
-  fileName = "YellowColors.css";
-  
-  reader.open(fileName, ios::in);
-}
+  do {
+   cout << "Please enter a file name to save your theme to (type the color you chose and add '.css' to the end of the color name): " << endl;
+   
+   cin >> fileName;
 
-else if(colorName == "Green"){
-  
-  fileName = "GreenColors.css";
+    if(fileName != "Red.css" && fileName != "Yellow.css" && fileName != "Green.css" && fileName != "Blue.css" && fileName != "Purple.css"){
+
+      cout << "The file name you entered is not valid. Please re-enter the file name to have the color you chose and '.css' after the color name." << endl;
+    }
+
+  } while (fileName != "Red.css" && fileName != "Yellow.css" && fileName != "Green.css" && fileName != "Blue.css" && fileName != "Purple.css");
+
+ cout << "You chose to save the " << colorName << " theme to the file named " << fileName << "." << endl;
+
+  if(fileName == "Red.css"){
   
   reader.open(fileName, ios::in);
-}
-
-else if(colorName == "Blue"){
+  }
   
-  fileName = "BlueColors.css";
+  else if(fileName == "Yellow.css"){
   
   reader.open(fileName, ios::in);
-}
+  }
 
-else if(colorName == "Purple"){
-  
-  fileName = "PurpleColors.css";
+  else if(fileName == "Green.css"){
   
   reader.open(fileName, ios::in);
-}
+  }
 
-if (reader.is_open()){
-  cout << "The file for " << colorName << " colors is open." << endl;
-}
+  else if(fileName == "Blue.css"){
+  
+  reader.open(fileName, ios::in);
+  }
+
+  else if(fileName == "Purple.css"){
+  
+  reader.open(fileName, ios::in);
+  }
+
+  if (reader.is_open()){
+    cout << "The file for " << colorName << " colors is open." << endl;
+  }
+
+  else {
+    cout << "No file has been opened." << endl;
+  }
+
+  reader.close();
 
   return 0;
 }
